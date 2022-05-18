f760718e2d6857998bb1ca3285e43e087417d553
Arduinum <message.chaos628@gmail.com>
2022-05-18 19:22:41 +0300

    added homework for lesson5

--- client_server_app/client.py
+++ client_server_app/client.py
@@ -4,13 +4,17 @@ from socket import socket, AF_INET, SOCK_STREAM
 from json import JSONDecodeError
 from time import ctime
 from common.utils import send_message, get_message, read_conf
-
+from logging import getLogger
+import logs.client_log_config
 
 name = './common/config.yaml'
+
+client_logger = getLogger('client')
 
 
 def request_presence(account='Guest', conf_name=name):
     """Запрашивает присутствие клиента"""
+    client_logger.debug('Выполняется запрос присутствия клиента')
     conf = read_conf(conf_name)
     output = dict()
     output[conf['ACTION']] = conf['PRESENCE']
@@ -21,38 +25,52 @@ def request_presence(account='Guest', conf_name=name):
 
 def response_analysis(message, conf_name=name):
     """Выполняет разбор ответа сервера"""
+    client_logger.debug(f'Выполняется разбор ответа сервера - {message}')
     conf = read_conf(conf_name)
     for key in message.keys():
         if key == conf['RESPONSE']:
             if message[conf['RESPONSE']] == 200:
+                client_logger.info('Получен ответ от сервера - 200 : OK')
                 return '200 : OK'
+            client_logger.error(f'Получен ответ с ошибкой от сервера - 400 : {message[conf["ERROR"]]}')
             return f'400 : {message[conf["ERROR"]]}'
+    client_logger.critical('Ошибка данных ValueError')
     raise ValueError
 
 
 def get_answer(serv_sock, conf_name=None):
     """Функция для получения ответа от сервера в правильной кодировке"""
+    client_logger.debug('Попытка получения сообщения из сокета в правильной кодировке')
     try:
         if conf_name is not None:
-            return response_analysis(get_message(serv_sock, conf_name=conf_name), conf_name=conf_name)
-        return response_analysis(get_message(serv_sock))
+            message_ok = response_analysis(get_message(serv_sock, conf_name=conf_name), conf_name=conf_name)
+            client_logger.info(f'Сообщение в правильной кодировке получено - {message_ok}')
+            return message_ok
+        message_ok = response_analysis(get_message(serv_sock))
+        client_logger.info(f'Сообщение в правильной кодировке получено - {message_ok}')
+        return message_ok
     except (ValueError, JSONDecodeError):
+        client_logger.error('Провал декодирования сообщения сервера!')
         return 'Провал декодирования сообщения сервера!'
 
 
 def data_connect_serv(conf_name=name):
     """Получает корректные данные для соединения с сервером"""
+    client_logger.debug('Получение корректных данных дял соединения с сервером')
     conf = read_conf(conf_name)
     try:
         addr_server = argv[1]
         port_server = int(argv[2])
         if port_server < 1024 or port_server > 65535:
             raise ValueError
+        client_logger.info(f'Получены корректные адрес сервера {addr_server} и порт сервера {port_server}')
         return {'addr_server': addr_server, 'port_server': port_server}
     except IndexError:
+        client_logger.info(f'Установлены адрес сервера {conf["ADDR_DEF"]} и порт сервера {conf["PORT_DEF"]} по умолчанию так '
+                    f'как они не были введены')
         return {'addr_server': conf['ADDR_DEF'], 'port_server': conf['PORT_DEF']}
     except ValueError:
-        print('Номер порта должен быть указан в диапазоне от 1024 до 65535!')
+        client_logger.error('Номер порта должен быть указан в диапазоне от 1024 до 65535!')
         exit(1)
 
 
@@ -62,8 +80,11 @@ def work_client(conf_name=name):
     data_connect_server = data_connect_serv(conf_name)
     server_sock.connect((data_connect_server['addr_server'], data_connect_server['port_server']))
     message_to_serv = request_presence(conf_name=conf_name)
+    client_logger.info(f'Сообщение для сервера сформировано успешно')
     send_message(server_sock, message_to_serv, conf_name)
+    client_logger.info(f'Сообщение для сервера отправлено успешно')
     answer = get_answer(server_sock)
+    client_logger.info(f'Пришёл ответ от сервера - {answer}')
     print(answer)
 
 
--- client_server_app/server.py
+++ client_server_app/server.py
@@ -3,12 +3,18 @@ from sys import argv, exit
 from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from common.utils import get_message, send_message, read_conf
 from json import JSONDecodeError
+from logging import getLogger
+import logs.server_log_config
+
 
 name = './common/config.yaml'
+
+server_logger = getLogger('server')
 
 
 def message_handler(message, conf_name=name):
     """Обрабатывает сообщения от клиентов"""
+    server_logger.debug(f'Обработка сообщения от клиента - {message}')
     conf = read_conf(conf_name)
     if conf['ACTION'] in message and message[conf['ACTION']] == conf['PRESENCE'] and conf['TIME'] in message \
             and conf['USER_NAME'] in message and message[conf['USER_NAME']][conf['ACCOUNT_NAME']] == 'Guest':
@@ -21,31 +27,37 @@ def message_handler(message, conf_name=name):
 
 def get_address(conf_name=name):
     """Возращает адрес для прослушивания сервером"""
+    server_logger.debug(f'Получение адреса для прослушивания сервером из - {conf_name}')
     conf = read_conf(conf_name)
     try:
         for i, addr in enumerate(argv):
             if addr == '-a':
+                server_logger.info(f'Получен адрес для прослушивания сервером - {argv[i + 1]}')
                 return argv[i + 1]
+        server_logger.info('Для прослушивания сервером задан адрес по умолчанию - 0.0.0.0')
         return conf['ADDR_LISTEN_DEF']
     except IndexError:
-        print('После параметра "-a" нужно указать адрес для прослушивания сервером!')
+        server_logger.critical('После параметра "-a" нужно указать адрес для прослушивания сервером!')
 
 
 def get_port(conf_name=name):
     """Возращает корректный порт для прослушивания сервером"""
+    server_logger.debug(f'Получение порта для прослушивания сервером из - {conf_name}')
     conf = read_conf(conf_name)
     try:
         for i, port in enumerate(argv):
             if port == '-p':
                 if int(argv[i + 1]) < 1024 or int(argv[i + 1]) > 65535:
                     raise ValueError
+                server_logger.info(f'Получен порт для прослушивания сервером - {argv[i + 1]}')
                 return int(argv[i + 1])
+        server_logger.info(f'Для прослушивания сервером задан порт по умолчанию - {conf["PORT_DEF"]}')
         return conf['PORT_DEF']
     except IndexError:
-        print('После параметра "-p" нужно указать номер порта!')
+        server_logger.critical('После параметра "-p" нужно указать номер порта!')
         exit(1)
     except ValueError:
-        print('Номер порта должен быть указан в диапазоне от 1024 до 65535!')
+        server_logger.critical('Номер порта должен быть указан в диапазоне от 1024 до 65535!')
         exit(1)
 
 
@@ -63,12 +75,16 @@ def work_server(conf_name=name):
         client, client_addr = server_sock.accept()
         try:
             message_for_client = get_message(client)
+            server_logger.debug(f'Поступило сообщение от клиента - {message_for_client}')
             print(message_for_client)
             response = message_handler(message_for_client)
+            server_logger.info(f'Создан ответ для клиента - {response}')
             send_message(client, response)
+            server_logger.info('Сообщение клиенту отправлено успешно')
             client.close()
+            server_logger.debug('Соединение закрыто')
         except (ValueError, JSONDecodeError):
-            print('Поступило некорректное сообщение от клиента!')
+            server_logger.error('Поступило некорректное сообщение от клиента!')
 
 
 if __name__ == '__main__':
