93e53bb5c7feb91c322963b21b99d07cbd65da28
Arduinum <message.chaos628@gmail.com>
2022-06-25 11:37:42 +0300

    added homework for lesson4_QT part1

---
+++ client_server_app/client_storage.py
@@ -0,0 +1,147 @@
+from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime
+from sqlalchemy.orm import sessionmaker, declarative_base
+from datetime import datetime
+
+
+class ClientStorage:
+    """Класс для работы с базой данных клиента в декларативном стиле"""
+    Base = declarative_base()
+
+    class FamousUsers(Base):
+        """Класс известные для клиента пользователи"""
+        __tablename__ = 'famous_users'
+        id = Column(Integer, primary_key=True)
+        login = Column(String, unique=True)
+
+        def __init__(self, login):
+            self.login = login
+
+    class HistoryMessages(Base):
+        """Класс история сообщений"""
+        __tablename__ = 'history_messages'
+        id = Column(Integer, primary_key=True)
+        from_user = Column(String)  # от кого сообщение
+        for_user = Column(String)  # для кого сообщение
+        message = Column(Text)
+        date = Column(DateTime)
+
+        def __init__(self, from_user, for_user, message):
+            self.from_user = from_user
+            self.for_user = for_user
+            self.message = message
+            self.date = datetime.now()
+
+    class UserContacts(Base):
+        """Класс контакты пользователя"""
+        __tablename__ = 'user_contacts'
+        id = Column(Integer, primary_key=True)
+        contact_login = Column(String, unique=True)
+
+        def __init__(self, contact_login):
+            self.contact_login = contact_login
+
+    def __init__(self):
+        # установка соединения с бд и сбор конф информации
+        # echo=True - ведение лога, poll_recycle=7200 - переустановка соединения с бд каждые 2 часа
+        self.engine = create_engine('sqlite:///client_data.db3', echo=True, pool_recycle=7200)
+        self.Base.metadata.create_all(self.engine)  # создаём все таблицы
+        session_fabric = sessionmaker(bind=self.engine)
+        self.session = session_fabric()  # создаём сессию
+
+        self.session.query(self.UserContacts).delete()
+        self.session.commit()
+
+    def table_clear(self, command):
+        """Метод класса для очищения таблиц от данных"""
+        try:
+            if command == 'all':
+                self.session.query(self.FamousUsers).delete()
+                self.session.query(self.HistoryMessages).delete()
+                self.session.query(self.UserContacts).delete()
+                self.session.commit()
+            else:
+                eval(f'self.session.query(self.{command}).delete()')
+                self.session.commit()
+        except (Exception, ) as err:
+            print(f'Ошибка - {err} при работе с данными таблицы!')
+
+    def add_contact(self, user):
+        """Метод класса для добавления нового контакта"""
+        try:
+            if not self.session.query(self.UserContacts).filter_by(contact_login=user).count():
+                new_contact = self.UserContacts(user)
+                self.session.add(new_contact)
+                self.session.commit()
+        except (Exception, ) as err:
+            print(f'Ошибка - {err} при работе с данными таблицы!')
+
+    def delete_contact(self, user):
+        """Метод класса для удаления контакта из контактов клиента"""
+        try:
+            del_contact = self.session.query(self.UserContacts).filter_by(contact_login=user)
+            if del_contact.count():
+                del_contact.delete()
+                self.session.commit()
+        except (Exception, ) as err:
+            print(f'Ошибка - {err} при работе с данными таблицы!')
+
+    def add_users(self, users_list):
+        """Метод класса для добавления известных пользователей"""
+        try:
+            self.session.query(self.FamousUsers).delete()
+            for user in users_list:
+                if not self.session.query(self.FamousUsers).filter_by(login=user).count():
+                    new_user = self.FamousUsers(user)
+                    self.session.add(new_user)
+                    self.session.commit()
+        except (Exception, ) as err:
+            print(f'Ошибка - {err} при работе с данными таблицы!')
+
+    def save_message(self, from_user, for_user, message):
+        """Метод класса для сохранения сообщения"""
+        try:
+            new_message = self.HistoryMessages(from_user, for_user, message)
+            self.session.add(new_message)
+            self.session.commit()
+        except (Exception, ) as err:
+            print(f'Ошибка - {err} при работе с данными таблицы!')
+
+    def get_user_contacts(self):
+        """Метод класса возвращает список контактов пользователя"""
+        return [user[0] for user in self.session.query(self.UserContacts.contact_login).all()]
+
+    def get_users(self):
+        """Метод класса возращает список известных пользователей"""
+        return [user[0] for user in self.session.query(self.FamousUsers.login).all()]
+
+    def checker_contact(self, contact_login):
+        """Метод класса проверяет есть ли контакт в контактах"""
+        if self.session.query(self.UserContacts).filter_by(contact_login=contact_login).count():
+            return True
+        else:
+            return False
+
+    def get_history_messages(self, from_who=None, for_who=None):
+        """Метод класса возращает историю сообщений"""
+        history = self.session.query(self.HistoryMessages)
+        if for_who:
+            history = history.filter_by(for_user=for_who)
+        if from_who:
+            history = history.filter_by(from_user=from_who)
+        return [(history_line.from_user, history_line.for_user, history_line.message, history_line.date)
+                for history_line in history.all()]
+
+
+if __name__ == '__main__':
+    client_storage = ClientStorage()
+    client_storage.add_contact('bot628')
+    client_storage.add_contact('botT1000')
+    client_storage.add_contact('WalleT34')
+    client_storage.delete_contact('bot628')
+    print(client_storage.get_user_contacts())
+    client_storage.add_users(['bot628', 'botT1000', 'WalleT34'])
+    print(client_storage.get_users())
+    client_storage.save_message('WalleT34', 'botT1000', 'Hello botT1000')
+    print(client_storage.checker_contact('botT1000'))
+    print(client_storage.get_history_messages(from_who='WalleT34'))
+    # client_storage.table_clear('all')
--- client_server_app/server_storage.py
+++ client_server_app/server_storage.py
@@ -48,6 +48,30 @@ class ServerStorage:
             self.ip_address = ip_address
             self.port = port
 
+    class UsersContacts(Base):
+        """Класс контакты пользователей"""
+        __tablename__ = 'users_contacts'
+        id = Column(Integer, primary_key=True)
+        login_id = Column(ForeignKey('all_users.id'))
+        contact_login_id = Column(ForeignKey('all_users.id'))
+
+        def __init__(self, login_id, contact_login_id):
+            self.login_id = login_id
+            self.contact_login_id = contact_login_id
+
+    class UsersHistory(Base):
+        """Класс история действий пользователей"""
+        __tablename__ = 'users_history'
+        id = Column(Integer, primary_key=True)
+        login_id = Column(ForeignKey('all_users.id'))
+        transmitted = Column(Integer)
+        accepted = Column(Integer)
+
+        def __init__(self, login_id):
+            self.login_id = login_id
+            self.transmitted = 0  # переданных сообщений
+            self.accepted = 0  # полученных сообщений
+
     def __init__(self):
         # установка соединения с бд и сбор конф информации
         # echo=True - ведение лога, poll_recycle=7200 - переустановка соединения с бд каждые 2 часа
@@ -70,6 +94,8 @@ class ServerStorage:
                 user = self.AllUsers(login)
                 self.session.add(user)
                 self.session.commit()
+                user_in_history = self.UsersHistory(user.id)
+                self.session.add(user_in_history)
 
             user_active = self.ActiveUsers(user.id, datetime.now(), ip_address, port)
             self.session.add(user_active)
@@ -116,19 +142,87 @@ class ServerStorage:
 
     def table_clear(self, command):
         """Метод класса для очищения таблиц от данных"""
-        if command == 'all':
-            self.session.query(self.AllUsers).delete()
-            self.session.query(self.ActiveUsers).delete()
-            self.session.query(self.HistoryLogins).delete()
-            self.session.commit()
-        else:
-            eval(f'self.session.query(self.{command}).delete()')
-            self.session.commit()
+        try:
+            if command == 'all':
+                self.session.query(self.AllUsers).delete()
+                self.session.query(self.ActiveUsers).delete()
+                self.session.query(self.HistoryLogins).delete()
+                self.session.commit()
+            else:
+                eval(f'self.session.query(self.{command}).delete()')
+                self.session.commit()
+        except (Exception, ) as err:
+            print(f'Ошибка - {err} при работе с данными таблицы!')
 
+    def working_message(self, addresser, receiver):
+        """Метод класса для фиксации передачи сообщения и делает об этом отметки в бд"""
+        addresser = self.session.query(self.AllUsers).filter_by(login=addresser).first().id  # отправитель сообщения
+        receiver = self.session.query(self.AllUsers).filter_by(login=receiver).first().id  # получатель сообщения
+        addresser_line = self.session.query(self.UsersHistory).filter_by(login_id=addresser).first()
+        addresser_line.transmitted += 1
+        receiver_line = self.session.query(self.UsersHistory).filter_by(login_id=receiver).first()
+        receiver_line.accepted += 1
+        self.session.commit()
+
+    def add_contact(self, user, contact_user):
+        """Метод класса для добавления контакта из бд"""
+        user = self.session.query(self.AllUsers).filter_by(login=user).first()
+        contact_user = self.session.query(self.AllUsers).filter_by(login=contact_user).first()
+
+        if not contact_user:
+            return
+        if self.session.query(self.UsersContacts).filter_by(login_id=user.id, contact_login_id=contact_user.id).count():
+            return
+
+        contact_line = self.UsersContacts(user.id, contact_user.id)
+        self.session.add(contact_line)
+        self.session.commit()
+
+    def delete_contact(self, user, contact_user):
+        """Метод класса для удаления контакта из бд"""
+        user = self.session.query(self.AllUsers).filter_by(login=user).first()
+        contact_user = self.session.query(self.AllUsers).filter_by(login=contact_user).first()
+
+        if not contact_user:
+            return
+
+        self.session.query(self.UsersContacts).filter(
+            self.UsersContacts.login_id == user.id,
+            self.UsersContacts.contact_login_id == contact_user.id
+        ).delete()
+        self.session.commit()
+
+    def login_history(self, login=None):
+        """Метод класса возвращающий входов для одного пользователя или для всех"""
+        history = self.session.query(self.AllUsers.login,
+                                   self.HistoryLogins.entry_time,
+                                   self.LoginHistory.ip_address,
+                                   self.LoginHistory.port
+                                   ).join(self.AllUsers)
+        if login:
+            history = history.filter(self.AllUsers.login == login)
+        return history
+
+    def get_users_contacts(self, login):
+        """Метод класса возвращающий список контактов пользователя"""
+        user = self.session.query(self.AllUsers).filter_by(login=login).one()
+        users_contacts = self.session.query(self.UsersContacts, self.AllUsers.id). \
+            filter_by(login_id=user.id).join(self.AllUsers, self.UsersContacts.contact_login_id == self.AllUsers.id)
+        return [contact_user[1] for contact_user in users_contacts.all()]
+
+    def get_message_count(self):
+        """Метод класса возвращающий колличество переданных и полученных сообщений"""
+        message_count = self.session.query(
+            self.AllUsers.login,
+            self.AllUsers.last_entry_time,
+            self.UsersHistory.transmitted,
+            self.UsersHistory.accepted
+        ).join(self.AllUsers)
+        return message_count
 
 if __name__ == '__main__':
     storage = ServerStorage()
-    # storage.table_clear('all')
+    storage.table_clear('all')
     # storage.table_clear('AllUsers')
     storage.user_login('Bot228', '0.0.0.0', 7777)
     storage.user_login('BotT1000', '0.0.0.1', 7777)
@@ -143,3 +237,8 @@ if __name__ == '__main__':
     print(history)
     history = storage.get_list_data('history', 'BotT1000')
     print(history)
+    storage.working_message('Bot228', 'BotT1000')
+    storage.add_contact('BotT1000', 'BotT1000')
+    storage.add_contact('Bot228', 'Bot228')
+    storage.delete_contact('BotT1000', 'BotT1000')
+    # print(storage.login_history()) - становился на нём выдаёт ошибку!
